341f45403846828cb1fe85054672ca5e5ac2cb61
laumedina03 <laura.medina03@usa.edu.co>
2024-08-29 15:49:35 +0000

    update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import 
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+    
+if __name__ == ('__main__'):
+    app.run(port="5000", host = '0.0.0.0', debug = True)
